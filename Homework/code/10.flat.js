d7d9286141f1405e59dadf587ad19307f6af685f
StellaYangFan <1062878468@qq.com>
2019-10-14 10:23:31 +0800

    update promise.try method.

--- Homework/code/10.flat.js
+++ Homework/code/10.flat.js
@@ -30,23 +30,39 @@
 Array.prototype.flat = function(n = 1) {
   let current = this;
   let count = 0;
-  
   if(current == null) throw new Error('Cannot convert undefined or null to object');
-
   if (n === 0 || !current.length) return current;
-
   try {
     while(count++ < n) {
-      if (!current.some(item => Array.isArray(item))) return current;
+      if (!current.some(Array.isArray)) return current;
       current = current.reduce((prev, next)=> prev.concat(next), []);
     }
   }catch(error) {
     throw error;
   }
-
   return current;
 }
 
 // Array.prototype.flat.call("tom");
 let arr = [1, [1, [3, [4, [5]]]]];
 console.log(arr.flat(Infinity));
+
+// 对比
+Array.prototype.flat = function( times = 1 ) {
+  function flatOneFloor(arr) {
+      return arr.reduce( function(prev,next){
+          if(Array.isArray(next)) {
+              return [...prev,...next]
+          } else {
+              return [...prev,next]
+          }
+      },[])
+  }
+  if( --times == 0 ) {
+      return flatOneFloor(this)
+  } else {
+      return flatOneFloor(this).flat( times )
+  }
+}
+let r1 = [1,[2,[3,[4,[5]]]]].flat(3);
+console.log(r1); 
--- Homework/code/4.Promise.try.js
+++ Homework/code/4.Promise.try.js
@@ -1,16 +1,30 @@
 //  Promise.try实现
 
-Reflect.defineProperty(Promise.prototype, "try", {
-    value: function(fn) {
+// Reflect.defineProperty(Promise.prototype, "try", {
+//     value: function(fn) {
 
-    }
-})
+//     }
+// })
 
-// 验证
-function add(a, b) {
-    console.log(a + b);
+// // 验证
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// Promise.try(() => add(1, 2));
+// Promise.try(setTimeout(() => console.log("timeout")));
+// console.log("end");
+
+
+// Update 1
+Promise.try = function(fn) {
+  return (async ()=> fn())()
 }
 
-Promise.try(() => add(1, 2));
-Promise.try(setTimeout(() => console.log("timeout")));
-console.log("end");
+function fn () {
+  throw new Error("Something wrong.");
+}
+
+Promise.try(fn);
+
+console.log("OK");
