c51cef5e1879370a52babcff49676f8250a3b8dd
StellaYangFan <1062878468@qq.com>
2019-10-18 09:24:20 +0800

    update flat

--- Homework/code/10.flat.js
+++ Homework/code/10.flat.js
@@ -26,23 +26,6 @@
 // arr.flat(Infinity); 
 // [1, 2, 3, 4, 5]
 
-// 成熟版
-Array.prototype.flat = function(n = 1) {
-  let current = this;
-  let count = 0;
-  if(current == null) throw new Error('Cannot convert undefined or null to object');
-  if (n === 0 || !current.length) return current;
-  try {
-    while(count++ < n) {
-      if (!current.some(Array.isArray)) return current;
-      current = current.reduce((prev, next)=> prev.concat(next), []);
-    }
-  }catch(error) {
-    throw error;
-  }
-  return current;
-}
-
 // Array.prototype.flat.call("tom");
 let arr = [1, [1, [3, [4, [5]]]]];
 console.log(arr.flat(Infinity));
@@ -66,3 +49,23 @@ Array.prototype.flat = function( times = 1 ) {
 }
 let r1 = [1,[2,[3,[4,[5]]]]].flat(3);
 console.log(r1); 
+
+// 更新版
+Array.prototype.flat = function(n = 1) {
+  let current = this;
+  let count = 0;
+  if(current == null) throw new Error('Cannot convert undefined or null to object');
+  if (n === 0 || !current.length) return current;
+  try {
+    while(count++ < n) {
+      if (!current.some(Array.isArray)) return current;
+      current = current.reduce((prev, next)=> prev.concat(next), []);
+    }
+  }catch(error) {
+    throw error;
+  }
+  return current;
+}
+
+arr = [1, [2, [3, [4, [5]]]]];
+arr = arr.flat(Infinity);
