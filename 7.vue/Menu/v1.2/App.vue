0ff220ef0c9c64ba6f4cd84b6749075828dd9e99
StellaYangFan <1062878468@qq.com>
2019-10-25 09:24:26 +0800

    update Menu

---
+++ 7.vue/Menu/v1.2/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Menu :data="data"></Menu>
+  </div>
+</template>
+
+<script>
+import Menu from "./Menu";
+let {data} = require("./data.json");
+export default {
+  data(){
+    return { data }
+  },
+  components: { Menu } 
+}
+</script>
