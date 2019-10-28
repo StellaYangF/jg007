ac2d3236d6048ad15d7e5e78f5306fc47f5106ea
StellaYangFan <1062878468@qq.com>
2019-10-28 15:29:43 +0800

    tsx-tree not completed

--- 9.react/1.tree.tsx/src/index.tsx
+++ 9.react/1.tree.tsx/src/index.tsx
@@ -1,1 +1,6 @@
-console.log("Bonjour!");
+import React from "react";
+import ReactDOM from "react-dom";
+import Tree from "./components/tree";
+import data from "./data";
+
+ReactDOM.render(,document.getElementById("root"));
