b50b98f53cb0c0dd7903b00d156ec90d39b9cd6a
StellaYangFan <1062878468@qq.com>
2019-10-28 15:05:35 +0800

    __tests__ added

---
+++ 9.react/1.tree.tsx/src/__tests__/utils.spec.tsx
@@ -0,0 +1,9 @@
+import { sum } from '../utils';
+describe('sum', () => {
+    test('1+1', () => {
+        expect(sum(1, 1)).toBe(2);
+    });
+    test('2+2', () => {
+        expect(sum(2, 2)).toBe(4);
+    });
+});
