30a92ffa7b145f3b112f64a60b5a11e4d39a522a
StellaYangFan <1062878468@qq.com>
2019-10-29 14:11:19 +0800

    delete original version

--- 9.react/1.tree.tsx/src/components/tree.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import "./index.less";
-import { TreeData }
--- 9.react/1.tree.tsx/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Tree from "./components/tree";
-import data from "./data";
-
-ReactDOM.render(,document.getElementById("root"));
