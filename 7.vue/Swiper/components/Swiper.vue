f7044420d64d42485c585b09507f3c8c18684bed
StellaYangFan <1062878468@qq.com>
2019-10-29 13:48:58 +0800

    update Swiper

--- 7.vue/Swiper/components/Swiper.vue
+++ 7.vue/Swiper/components/Swiper.vue
@@ -10,7 +10,7 @@
     <div class="viewport">
       <slot></slot>
     </div>
-    <!-- dots -->
+    <!-- 点 dots -->
     <div class="dots">
       <span
         v-for="(item,index) in len"
@@ -20,141 +20,169 @@
       >{{item}}</span>
     </div>
     <div class="btn-list">
-      <button @click="select(active - 1)">Left</button>
-      <button @click="select(active + 1)">Right</button>
+      <button @click="select(active-1)">左边</button>
+      <button @click="select(active+1)">右边</button>
     </div>
   </div>
 </template>
+
 <script>
-export default {
+export default { // compositionApi
   props: {
     autoplay: {
       type: Boolean,
-      default: true,
+      default: true
     },
     value: {
       type: String,
-      default: "",
+      default: ""
     }
   },
   data() {
-    return {
-      currentSelected: "",
-      len: 0,
-    }
+    return { currentSelcted: "", len: 0 };
   },
   methods: {
+    touchstart(e) {
+      this.mouseenter(); // 停止滚动
+      this.startX = e.touches[0].clientX;
+    },
+    touchmove() {
+      console.log("移动");
+    },
+    touchend(e) {
+       let endX =  e.changedTouches[0].clientX;
+       let distance = endX - this.startX;
+
+       // 更严格的判断
+       if(distance < 0){
+           this.select(this.active+1)
+       }else{
+           this.select(this.active- 1)
+       }
+       this.run(); // 当位置变化后 还需要继续开启定时器
+    },
+    select(newIndex) {
+      //可以控制切换功能
+      // 点击dots 时 会触发对应的更新事件
+
+      this.prevPosition = this.active; // 当选择时
+
+      if (newIndex === this.names.length) newIndex = 0; // 做左右点击
+      if (newIndex === -1) newIndex = this.names.length - 1;
+      this.$emit("input", this.names[newIndex]); // 改变后发射即可
+    },
     mouseenter() {
       clearInterval(this.timer);
       this.timer = null;
     },
     mouseleave() {
+      // 如果有timer  表示已经在跑了 就不管了
       if (!this.timer) {
         this.run();
       }
     },
-    touchstart(e) {
-      this.mouseenter();
-      this.startX = e.targets[0].clientX;
-    },
-    touchmove() {},
-    touchend(e) {
-      let endX = e.changedTouches[0].clientX;
-      let distance = endX - this.startX;
-
-      if (distance < 0) {
-        this.select(this.active + 1)
-      } else{
-        this.select(this.active - 1)
-      };
-      this.run();
-    },
-    select(newIndex) {
-      this.prevPosition = this.active;
-      if (newIndex === this.names.length) newIndex = 0;
-      if (newIndex === -1) newIndex = this.names.length - 1;
-      this.$emit("input", this.names[newIndex]);
-    },
     showChild() {
-      this.currentSelected = this.value || this.$children[0].name;
+      // 切换显示组件
+      // 需要获取当前从谁开始 从哪个name属性开始
+      this.currentSelcted = this.value || this.$children[0].name;
+      // 1) 让对应的儿子显示 其他人隐藏
       this.$children.forEach(vm => {
-        this.$nextTick(()=> {
-          vm.selected = this.currentSelected;
+        this.$nextTick(() => {
+          // 我们需要更改完毕 是正向的还是反向的 再去更新视图
+          vm.selected = this.currentSelcted; // 跟子组件说那个需要加载
         });
-        let reverse = this.prevPosition > this.value ? true : false;
+
+        // 要跟儿子说 你是正着走还是反着走
+        //                      1        2
+        let reverse = this.prevPosition > this.active ? true : false;
+        // 和 子节点说 是正的还是反的
         vm.reverse = reverse;
+
+        // 考虑临街值的问题
         if (this.timer) {
-          if (this.prevPosition === 0 && this.active === this.len -1) {
+          // 无缝的时候
+          if (this.prevPosition === 0 && this.active === this.len - 1) {
             vm.reverse = true;
           }
-          if (this.prevPosition === this.len -1 && this.active === 0) {
+          if (this.prevPosition === this.len - 1 && this.active === 0) {
             vm.reverse = false;
           }
         }
-      })
+      });
     },
     run() {
       if (this.autoplay) {
         this.timer = setInterval(() => {
+          // 把索引往后调整 box1 box2 之后把调整的结果 发送给面
+          // 确定当前是第几个
           let index = this.active;
-          let newIndex = index -1;
+          let newIndex = index - 1;
           this.select(newIndex);
-        }, 3000)
+        }, 3000);
       }
-    },
-  },
-
-  computed: {
-    active() {
-      return this.names.indexOf(this.currentSelected);
     }
   },
-
+  computed: {
+    active() {
+      // active 表示当前是第几个的索引
+      return this.names.indexOf(this.currentSelcted);
+    }
+  },
   beforeDestroy() {
     clearInterval(this.timer);
   },
-
   watch: {
     value() {
       this.showChild();
     }
   },
-
+  // render
   mounted() {
+    // 1) 只执行一次
     this.names = this.$children.map(child => child.name);
-    this.len = this.name.length;
-    this.showChild();
-  },
-}
+    this.len = this.names.length; // 获取儿子节点的个数
+    this.showChild(); // 1) 显示默认的节点
+    // 2） 自动轮播
+    this.run();
+    // 3) 控制动画的方向
+    // 先记录当前的值
+    this.prevPosition = this.active; // 记录刚进来的值
+  }
+};
 </script>
 
 <style lang="stylus">
-  .swiper {
-    margin: 0 auto 
-    border: 10px solid purple 
-  }
-  .viewport {
-    position 
-    overflow hidden
-    height 150px
-  }
-  .dots {
-    display flex
-    justify-content center
-  }
-    .dots span  {
-      cursor pointer
-      width 30px
-      height 30px
-      text-align center
-      line-height 30px
-      border-radius 50%
-      border 1px solid #cccccc
-      margin 0 10px
-    }
-  .active {
-    background green 
-    color #fff
-  }
+.swiper {
+  margin: 0 auto;
+  border: 10px solid purple;
+}
 
+.viewport {
+  position: relative;
+  overflow: hidden;
+  height: 150px;
+}
+
+.dots {
+  display: flex;
+  justify-content: center;
+
+  span {
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+    border-radius: 50%;
+    border: 1px solid red;
+    margin: 0 10px;
+  }
+}
+
+.active {
+  background: red;
+  color: #fff;
+}
 </style>
+
+
--- 7.vue/Swiper/components/SwiperItem.vue
+++ 7.vue/Swiper/components/SwiperItem.vue
@@ -1,6 +1,7 @@
 <template>
   <transition>
-    <div class="swiper-itme" v-if="isShow" :class="{reverse}">
+    <!-- 可以控制div 隐藏显示 -->
+    <div class="swiper-item" v-if="isShow" :class="{reverse}">
       <slot></slot>
     </div>
   </transition>
@@ -9,35 +10,42 @@
 export default {
   computed: {
     isShow() {
+      // 根据用户的参数 来控制显示某一个
       return this.selected === this.name;
     }
   },
   data() {
-    return { selected:"", reverse: "" }
+    return { selected: "" ,reverse:''}; // 默认当前用户传递过来的数据
   },
   props: {
     name: {
       type: String,
-      required: true,
+      required: true
     }
   }
-}
+};
 </script>
-
 <style lang="stylus">
-  .v-enter-active, .v-leave-active 
-    transisiton all .5s linear
-  .v-leave-to
+.v-enter-active,.v-leave-active{
+    transition all .5s linear
+}
+.v-leave-to{
     transform translate(-100%)
-    .reverse
-      transform translate(100%)
-  .v-enter 
+}
+.v-enter{
+    transform translate(100%);
+}
+
+.v-leave-to.reverse{
     transform translate(100%)
-    .reverse
-      transform translate(-100%)
-  .v-enter-active
-    position absolute
-    width 100%
-    top 0
-    left 0
+}
+.v-enter.reverse{
+    transform translate(-100%);
+}
+.v-enter-active{
+    position absolute;
+    width 100%;
+    top:0;
+    left:0;
+}
 </style>
