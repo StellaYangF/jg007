948fb067623ba00fde16f0b7959297f3c468d6a4
StellaYangFan <1062878468@qq.com>
2019-11-04 10:53:43 +0800

    vuex test

--- 7.vue/7.vuex/vuex-project/src/App.vue
+++ 7.vue/7.vuex/vuex-project/src/App.vue
@@ -1,30 +1,10 @@
 <template>
-  <div id="app">
-     Tom的年龄是:{{$store.state.age}}
-     我的年龄是{{$store.getters.myAge}}
-
-     <br>
-     <button @click="add">按钮</button>
-
-      <button @click="minus">异步减少</button>
-      <p>computedC: {{this.$store.getters.computedC}}</p>
-      <p>module a: {{this.$store.state.a.a}}</p>
+  <div>
+    <p>Tom is {{$store.age}} years old.</p>
   </div>
 </template>
-
-
 <script>
 export default {
-  mounted(){
-    console.log(this.$store._modules)
-  },  
-  methods:{
-    add(){
-      this.$store.commit('syncAdd',10); // 载荷
-    },
-    minus(){
-      this.$store.dispatch('asyncMinus',5);
-    }
-  },
+  
 }
 </script>
--- 7.vue/7.vuex/vuex-project/src/main.js
+++ 7.vue/7.vuex/vuex-project/src/main.js
@@ -1,10 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from './App';
+import store from "./store";
 
 new Vue({
-  store, // 将store  new Vue.$store ,会在所有的组件中声明一个属性$store
-  render: h => h(App)
-}).$mount('#app')
+  el: "#app",
+  store,
+  render: h => h(App),
+})
